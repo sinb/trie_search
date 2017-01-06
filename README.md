ce9ffee1f68a1faa023c495c457c99e8a389c6e7
sinb <linkin816@gmail.com>
2017-01-06 11:09:34 +0800

    add keyword search

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 build a search engine with trie tree (prefix tree).
 
-support whole word search and prefix search.
+support whole word search and prefix search, and keyword search.
 
 ## example
 ```
     trie = build_trie_test_data("data.tsv")
     ## whole word search
-    print(trie.whole_search("禾盛新材:第四届董事会第十一次会议决议公告"))
+    results = trie.whole_search("禾盛新材:第四届董事会第十一次会议决议公告")
+    print_result(results)
+
     ## prefix search
-    results = trie.prefix_search("汇金")
-    if results:
-        for result in results:
-            print(result[0]),
-            print(result[1])
+    results = trie.prefix_search("金一文化")
+    print_result(results)
+
+    ## keyword search
+    results = trie.keyword_search("文化")
+    print_result(results)
+
 ```
--- triesearch.py
+++ triesearch.py
@@ -12,6 +12,7 @@ class TrieNode:
 class TrieTree:
     def __init__(self):
         self.root = TrieNode()
+        self.node_map = {}
 
     def insert(self, string, data):
         string = string.decode("utf-8")
@@ -21,6 +22,11 @@ class TrieTree:
             if not node:
                 node = TrieNode()
                 current_node.children[char] = node
+                if not self.node_map.get(char):
+                    self.node_map[char] = set()
+                    self.node_map[char].add(current_node)
+                else:
+                    self.node_map[char].add(current_node)
             current_node = node
         current_node.is_end = True
         current_node.data = data
@@ -29,15 +35,17 @@ class TrieTree:
     def whole_search(self, string):
         string = string.decode("utf-8")
         current_node = self.root
+        result = []
         for char in string:
             node = current_node.children.get(char)
             if not node:
-                return None
+                return []
             current_node = node
         if current_node.is_end:
-            return current_node.data
+            result.append((string, current_node.data))
+            return result
         else:
-            return None
+            return []
 
     def prefix_search(self, string):
         string = string.decode("utf-8")
@@ -45,7 +53,7 @@ class TrieTree:
         for char in string:
             node = current_node.children.get(char)
             if not node:
-                return None
+                return []
             current_node = node
         result = []
         self.forward(node, result)
@@ -65,6 +73,26 @@ class TrieTree:
         else:
             result.append((node.key, node.data))
 
+    def prefix_search_from_node(self, string, current_node):
+        for char in string:
+            node = current_node.children.get(char)
+            if not node:
+                return []
+            current_node = node
+        result = []
+        self.forward(node, result)
+        return result
+
+    def keyword_search(self, string):
+        string = string.decode("utf-8")
+        char = string[0]
+        nodes = self.node_map.get(char)
+        result = []
+        if nodes:
+            for node in nodes:
+                result += self.prefix_search_from_node(string, node)
+        return result
+
 
 def build_trie_test_data(filename):
     trie = TrieTree()
@@ -75,13 +103,22 @@ def build_trie_test_data(filename):
                 trie.insert(data[0].strip(), data[1].strip())
     return trie
 
+
+def print_result(data):
+    for item in data:
+        print(item[0]),
+        print(item[1])
+
 if __name__ == "__main__":
     trie = build_trie_test_data("data.tsv")
     ## whole word search
-    print(trie.whole_search("禾盛新材:第四届董事会第十一次会议决议公告"))
+    results = trie.whole_search("禾盛新材:第四届董事会第十一次会议决议公告")
+    print_result(results)
+
     ## prefix search
     results = trie.prefix_search("金一文化")
-    if results:
-        for result in results:
-            print(result[0]),
-            print(result[1])
+    print_result(results)
+
+    ## keyword search
+    results = trie.keyword_search("文化")
+    print_result(results)
